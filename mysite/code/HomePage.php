aebd509f6f12ace7730e756e85638fdb668b489c
Ingo Schommer <ingo@silverstripe.com>
2013-10-01 11:56:11 +0200

    Updated demo to latest 3.1

--- mysite/code/HomePage.php
+++ mysite/code/HomePage.php
@@ -1,12 +1,6 @@
 <?php
 class HomePage extends Page {
 
-	public static $db = array(
-	);
-
-	public static $has_one = array(
-	);
-
 }
 class HomePage_Controller extends Page_Controller {
 
--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -1,9 +1,6 @@
 <?php
 class Page extends SiteTree {
 
-	private static $db = array(
-	);
-
 	private static $has_one = array(
 		"MyWidgetArea" => "WidgetArea",
 	);
@@ -18,24 +15,6 @@ class Page extends SiteTree {
 
 }
 class Page_Controller extends ContentController {
-
-	/**
-	 * An array of actions that can be accessed via a request. Each array element should be an action name, and the
-	 * permissions or conditions required to allow the user to access it.
-	 *
-	 * <code>
-	 * array (
-	 *     'action', // anyone can access this action
-	 *     'action' => true, // same as above
-	 *     'action' => 'ADMIN', // you must have ADMIN permissions to access this action
-	 *     'action' => '->checkAction' // you can only access this action if $this->checkAction() returns true
-	 * );
-	 * </code>
-	 *
-	 * @var array
-	 */
-	private static $allowed_actions = array (
-	);
 
 	public function init() {
 		parent::init();
