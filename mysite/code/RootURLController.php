59165ba199a35d62542fcd36669f3c53b07e2ef6
Damian Mooyman <damian@silverstripe.com>
2015-06-03 15:46:54 +1200

    Remove duplicate RootURLController

--- mysite/code/RootURLController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class RootURLController extends Controller {
-
-	function index() {
-		echo "<html>Your site is now set up. Start adding controllers to mysite to get started.</html>";
-	}
-
-}
