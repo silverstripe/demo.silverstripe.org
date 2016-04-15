41d37ce12f0f0cd727858126f6c26c2047f1214d
Cam Findlay <cam@silverstripe.com>
2016-04-15 13:23:20 +1200

    FEATURE Add GA to CMS to help improve understanding of use

--- mysite/_config.php
+++ mysite/_config.php
@@ -12,20 +12,3 @@ if(defined('SS_DATABASE_NAME') && SS_DATABASE_NAME) {
 
 require_once('conf/ConfigureFromEnv.php');
 
-MySQLDatabase::set_connection_charset('utf8');
-
-// Set the current theme. More themes can be downloaded from
-// http://www.silverstripe.org/themes/
-SSViewer::set_theme('simple');
-
-// Disable BasicAuth by default
-BasicAuth::protect_entire_site(false);
-
-// Set the site locale
-i18n::set_locale('en_US');
-
-// Enable nested URLs for this site (e.g. page/sub-page/)
-if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
-
-
-LeftAndMain::require_javascript('mysite/javascript/usabilla.js');
