715546c86e39ace6284275b17cd652b225a82688
Cam Findlay <cam@silverstripe.com>
2016-04-22 13:49:03 +1200

    CRON set reset of content every 20 mins.

---
+++ mysite/code/DemoCronTask.php
@@ -0,0 +1,25 @@
+<?php
+class DemoCronTask implements CronTask {
+
+    /**
+    *
+    * @return string
+    */
+    public function getSchedule()
+    {
+        return "*/20 * * * *";
+    }
+
+    /**
+    *
+    * @return void
+    */
+    public function process()
+    {
+        $this->request = Controller::curr()->getRequest();
+
+        //refresh demo site
+        $reset_task = new DemoResetTask();
+        $reset_task->run($this->request);
+    }
+}
