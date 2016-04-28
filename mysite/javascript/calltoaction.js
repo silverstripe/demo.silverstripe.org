f7879ed813b2a5a599fb6e399b29319791a37f94
Jared Lee <newleeland@gmail.com>
2016-04-29 11:09:52 +1200

    CMS call to action

---
+++ mysite/javascript/calltoaction.js
@@ -0,0 +1,5 @@
+(function($) {
+    $(document).ready(function(){
+        $('#cms-menu .cms-panel-content').append('<div class="demo-ctas"><a href="https://userhelp.silverstripe.org/en/3.3/?utm_source=demosite&utm_medium=referrallink&utm_content=CMS&utm_campaign=Demo1" target="_blank" class="cta-btn">Learn the CMS</a><a href="https://docs.silverstripe.org/en/3.3/?utm_source=demosite&utm_medium=referrallink&utm_content=developer&utm_campaign=Demo1" target="_blank" class="cta-btn">Learn the Framework</a><a href="http://www.silverstripe.org/footer-menu/get-in-touch/?utm_source=demosite&utm_medium=referrallink&utm_content=contactform&utm_campaign=Demo1" target="_blank" class="cta-btn">Get in touch</a><h3>Like what you see?</h3></div>');
+    })
+})(jQuery);
