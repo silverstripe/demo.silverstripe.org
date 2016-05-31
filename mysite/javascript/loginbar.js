d19e4019f79addc489992d7519e258acf67fcd42
Jared Lee <newleeland@gmail.com>
2016-05-31 16:44:23 +1200

    Revert "Fixes for rebased work to JS"
    This reverts commit 45abfd2e4b5a33c88e68fa07ea30f6642f1456fc.

--- mysite/javascript/loginbar.js
+++
@@ -1,8 +0,0 @@
-(function($) {
-	$(document).ready(function() {
-		$('select[name="Language"]').on('change', function() {
-			$('input[name="Email"]').val(this.value);
-			ga('send', 'event', 'Login', 'language-selector', this.value);
-		});
-	})
-})(jQuery);
