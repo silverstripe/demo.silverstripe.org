62006d64a00862a16fc9cb2854d1ca143f4d72fb
Sam Minnee <sam@silverstripe.com>
2017-06-29 18:00:28 +1200

    Removing the shim now that SSP 3.11 supports SS4

--- mysite/_config.php
+++ mysite/_config.php
@@ -2,10 +2,15 @@
 
 use SilverStripe\Control\Director;
 
-require_once __DIR__ . '/env-shiv.php';
-
 global $project;
 $project = 'mysite';
+
+// global $database;
+// if (defined('SS_DATABASE_NAME') && SS_DATABASE_NAME) {
+// 	$database = SS_DATABASE_NAME;
+// } else {
+// 	$database = 'SS_ss3demo';
+// }
 
 // if (defined('SS_CANONICAL_DOMAIN')) {
 // 	$allowedDomains = array(SS_CANONICAL_DOMAIN);
