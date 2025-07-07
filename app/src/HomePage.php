6f2c9d596909ded9fedf4507bc41fb3266d3055e
Steve Boyd <emteknetnz@gmail.com>
2025-07-07 21:28:18 +1200

    Add right panel fields

--- app/src/HomePage.php
+++ app/src/HomePage.php
@@ -1,18 +1,31 @@
 <?php
 
+use SilverStripe\Core\Config\Config;
 use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
 
 class HomePage extends Page
 {
     private static $db = [
-        'RightContent' => 'HTMLText'
+        'RightHeading' => 'Varchar',
+        'RightContent' => 'HTMLText',
+        'RightCtaLabel' => 'Varchar',
+        'RightCtaLink' => 'Varchar',
     ];
 
     public function getCMSFields()
     {
         $fields = parent::getCMSFields();
-        $field = HTMLEditorField::create('RightContent', 'Right Content');
-        $fields->addFieldToTab('Root.RightContent', $field);
+        $db = HomePage::config()->get('db', Config::UNINHERITED);
+        foreach ($db as $fieldName => $type) {
+            if ($type === 'Varchar') {
+                $field = $fields->dataFieldByName($fieldName);
+                $fields->removeByName($fieldName);
+                $fields->addFieldToTab('Root.RightContent', $field);
+            } elseif ($type === 'HTMLText') {
+                $field = HTMLEditorField::create('RightContent', 'Right Content');
+                $fields->addFieldToTab('Root.RightContent', $field);
+            }
+        }
         return $fields;
     }
 
